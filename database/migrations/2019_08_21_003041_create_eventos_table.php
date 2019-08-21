779ce85a4a79dcf25e8ca14cdaa2153de9e0dbdd
Manolo <manolomr221@gmail.com>
2019-08-20 19:56:00 -0500

    proyecto con migraciones y Auth(login) funcionando

---
+++ database/migrations/2019_08_21_003041_create_eventos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->dateTime('fecha');
+            $table->string('foto',200)->nullable();
+            $table->string('categoria');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+}
