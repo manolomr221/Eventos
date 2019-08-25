c0420d295c36e066298a3f55d3f1bfe00846f5c3
Manolo <manolomr221@gmail.com>
2019-08-25 17:14:59 -0500

    agregando validaciones y correccion de errores en crear evento

--- resources/views/Admin/crearEvento.blade.php
+++ resources/views/Admin/crearEvento.blade.php
@@ -1,49 +1,54 @@
 @extends('layouts.app')
 
-
+@section('content')
 <section id="content">
    
     <div class="container">
         <div class="row">
-            <div class="col-md-8 offset-md-2">
+         <!--   <div class="col-md-4"></div> -->
+            <div class="col-md-8 offset-md-2 ">
                     
+            <div class="card">
+                <div class="card-header text-center bg-info   "><strong>Registrar Evento</strong></div>
                 <form action="/Admin/registrar/evento" method="POST">
                 @csrf
                     <div class="form-group">
                         <label for="nombreEvento">Nombre del Evento</label>
-                        <input type="text" required="" name="nombre" class="form-control" id="nombreEvento" placeholder="co co co co como se llama esta mierda">
+                        <input type="text" required="" name="nombre" class="form-control " id="nombreEvento" placeholder="co co co co como se llama esta mierda">
                     </div>
                     <div class="form-group">
                         <label for="exampleFormControlTextarea1">Descripcion</label>
-                        <textarea class="form-control" name="descripcion" id="exampleFormControlTextarea1" placeholder="de que se trata esta mierda?" rows="3"></textarea>
+                        <textarea class="form-control" required="" name="descripcion" id="exampleFormControlTextarea1" placeholder="de que se trata esta mierda?" rows="3"></textarea>
                     </div>
                     <div class=" form-group custom-file">
-                        <input type="file"  lang="es">
+                        <input required="" type="file"  lang="es">
                        
                     </div>
                     
                     <div class="row">
                     <div class="col-md-6 ">
                     <label for="exampleFormControlInput1">fecha del Evento</label>
-                        <input type="date" class="form-control"  name="fecha" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <input type="date" required="" class="form-control"  name="fecha" id="exampleFormControlInput1" placeholder="name@example.com">
                     </div>
                         <div class="col-md-6">
                             <label for="exampleFormControlInput1">Categoria del Evento</label>
-                            <input type="text"  name="categoria" class="form-control" id="exampleFormControlInput1" placeholder="Categoria de Esta Mamada">
+                            <input type="text"  required="" name="categoria" class="form-control" id="exampleFormControlInput1" placeholder="Categoria de Esta Mamada">
                         </div>
                     </div> <br>
                    
                     <div class="form-group">
-                        <button type="submit" class="col-md-12 btn btn-primary">Submit</button>
+                        <button type="submit" class="col-md-6 offset-md-3 btn btn-success">Submit</button>
                     </div>
                    
                 </form>
+                <div class="card-body">
+                   
+                </div>
+            </div>
+              
             </div>
         </div>
     </div>
   </section>
-  
-
-
 
 @endsection
