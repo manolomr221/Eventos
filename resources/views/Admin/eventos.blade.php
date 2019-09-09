5199d0605a101531c6fb31110f6194749ef75df7
Manolo <manolomr221@gmail.com>
2019-09-08 22:16:40 -0500

    modificando iconos y agregando funcionalidad eliminar y editar

--- resources/views/Admin/eventos.blade.php
+++ resources/views/Admin/eventos.blade.php
@@ -84,60 +84,82 @@
 <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-            <div class="col-sm-9">
-                <div>
-                    <p class="profesor">eventos</p>
+    <div class="col-sm-9">
+        <div>
+            <p class="profesor">eventos</p>
+        </div>
+        <div>
+            <div class="col-sm-8 buscador">
+                <div class="input-group">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">Buscar</button>
+                    </span>
+                    <input type="text" class="form-control">
                 </div>
-                <div>
-                    <div class="col-sm-8 buscador">
-                        <div class="input-group">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">Buscar</button>
-                            </span>
-                            <input type="text" class="form-control">
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <table class="table table-hover">
-                                <thread>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>nombre</th>
-                                        <th>descripcion</th>
-                                        <th>fecha</th>
-                                        <th>categoria</th>
-                                        <th>Eliminar</th>
-                                        <th></th>
-                                        <th></th>
-                                    </tr>
-                                </thread>
-                                <tbody>
-                                    @foreach($eventos as $evento)
-                                    <tr>
+            </div>
+        </div>
+        <div>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <table class="table table-hover">
+                        <thread>
+                            <tr>
+                                <th>#</th>
+                                <th>nombre</th>
+                                <th>descripcion</th>
+                                <th>fecha</th>
+                                <th>categoria</th>
+                                <th>Eliminar</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thread>
+                        <tbody>
+                            @foreach($eventos as $evento)
+                                <tr>
                                     <th>{{$evento->id}}</th>
-                                        <th>{{$evento->nombre}}</th>
-                                        <th>{{$evento->descripcion}}</th>
-                                        <th>{{$evento->fecha}}</th>
-                                        <th>{{$evento->categoria}}</th>
-                                        
-                                         <th><button style="font-size:24px">Eliminar <i class="fa fa-exclamation-triangle"  style="font-size:48px;color:red"></i></button></th>                                       
-
-                                        <th><i class="fa fa-exclamation-triangle" style="font-size:48px;color:red"></i></th>
-                                        <th><button style="font-size:24px">Editar <i class="material-icons">create</i></th>
-                                        <th><i class="material-icons" style="font-size:48px;color:orange">create</i></th>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                                    <th>{{$evento->nombre}}</th>
+                                    <th>{{$evento->descripcion}}</th>
+                                    <th>{{$evento->fecha}}</th>
+                                    <th>{{$evento->categoria}}</th>
+                                    <th><i class="fa fa-trash" aria-hidden="true" value="{{$evento->id}}" style="font-size:48px;color:red"></i></th>
+                                    <th><i class="fa fa-pencil-square" aria-hidden="true" value="{{$evento->id}}" style="font-size:48px;color:orange"></i></th>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+
+<!-- modal seguridad eliminar evento-->
+<div class="modal fade" id="eliminarEvento" tabindex="-1" role="dialog" aria-labelledby="Eliminar Evento">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+           <p class="lead" style="text-align:center;">¿Estas seguro de eliminar éste Evento?</p>
+      </div>
+      <div class="modal-footer">
+        <form method="POST" action="" id="eliminarEvento">
+            {{ csrf_field() }}
+            <button type="submit" class="btn btn-danger" style="width:240px;">SI</button>
+        </form>
+        <button type="button" class="btn btn-success" style="width:50%;" data-dismiss="modal">NO</button>
+      </div>
+    </div>
+  </div>
 </div>
 
+<script>
+    $(document).ready(function(){
+        $('i.fa-pencil-square').click(function(){
+           window.location.href = '/Admin/evento/'+$(this).attr('value')+'/editar';
+        });
+         $('i.fa-trash').click(function(){
+           $('#eliminarEvento').modal('show');
+           $('form#eliminarEvento').attr('action','/Admin/evento/'+$(this).attr('value')+'/eliminar');
+         });
+    });
+</script>
 @endsection
